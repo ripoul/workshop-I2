1454d29d41108fce88eb9f13b06eb9760cc4bd5f
Jules LE BRIS <jls.lebris@gmail.com>
2019-11-25 15:34:52 +0100

    :sparkles: add webcam

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import Webcam from 'react-webcam';
 
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 
 const SecondPage = () => {
+  const webcamRef = React.useRef( null );
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    console.log( imageSrc );
+  }, [webcamRef]);
+  const videoConstraints = {
+    facingMode: { exact: 'environment' }
+  };
+
   return (
     <Layout pageName="two">
       <Helmet>
@@ -13,6 +24,10 @@ const SecondPage = () => {
       <Container className="content">
         <h1>Page Two</h1>
         <p>Welcome to page 2</p>
+        <Webcam ref={webcamRef} />
+        <button onClick={capture} videoConstraints={videoConstraints}>
+          Send CV for analyse
+        </button>
       </Container>
     </Layout>
   );
