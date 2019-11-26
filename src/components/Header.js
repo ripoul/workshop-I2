2922df17e086817a2b7cffff8dd1b838924acb5c
Raphaël TISON <13355624+Airthee@users.noreply.github.com>
2019-11-26 15:34:00 +0100

    :lipstick: Add links in header (#3)

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,33 +6,53 @@ import Container from 'components/Container';
 import Columns from 'components/Columns';
 import Column from 'components/Column';
 
-const Header = () => {
-  return (
-    <header>
-      <Container>
-        <Columns>
-          <Column>
-            <p>Workshop I2</p>
-          </Column>
-          <Column>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/webcam/">Webcam</Link>
-              </li>
-              <li>
-                <a href="https://github.com/ripoul/workshop-I2">
-                  <FaGithub />
-                </a>
-              </li>
-            </ul>
-          </Column>
-        </Columns>
-      </Container>
-    </header>
-  );
-};
+class Header extends React.Component {
+  state = {
+    links: [
+      {
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'Webcam',
+        link: '/webcam'
+      },
+      {
+        text: 'Mass import',
+        link: '/import'
+      },
+      {
+        text: <FaGithub />,
+        link: 'https://github.com/ripoul/workshop-I2'
+      }
+    ]
+  };
+
+  RenderLinks = () => {
+    const items = this.state.links.map(( l, i ) => (
+      <li key={i}>
+        <Link to={l.link}>{ l.text }</Link>
+      </li>
+    ));
+    return <ul>{ items }</ul>;
+  };
+
+  render() {
+    return (
+      <header>
+        <Container>
+          <Columns>
+            <Column>
+              <p>Workshop I2</p>
+            </Column>
+            <Column>
+              <this.RenderLinks />
+            </Column>
+          </Columns>
+        </Container>
+      </header>
+    );
+  }
+}
 
 export default Header;
