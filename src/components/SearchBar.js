f73ef6c3139437d9b021245ef7794e1503c78a9f
Raphaël TISON <13355624+Airthee@users.noreply.github.com>
2019-11-26 15:43:30 +0100

    :sparkles: Add search page (#2)
    * :lipstick: Initialize search
    
    Add search bar and list rendering
    
    * :triangular_flag_on_post: Fix search
    
    * :poop: apply correct code

---
+++ src/components/SearchBar.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FaSearch } from 'react-icons/fa';
+import axios from 'axios';
+
+class SearchBar extends React.Component {
+  static propTypes = {
+    onSearchResult: PropTypes.func
+  };
+
+  state = {
+    search: ''
+  };
+
+  handleInputChange = ( event ) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  onClickSearch = () => {
+    const api_host = process.env.API_HOST || 'http://127.0.0.1';
+    const url = `${api_host}/api/search`;
+
+    axios
+      .get( url, {
+        params: {
+          keyword: this.state.search
+        }
+      })
+      .then(( response ) => {
+        // Call callback with response
+        if ( typeof this.props.onSearchResult === 'function' ) this.props.onSearchResult( response.data );
+      });
+  };
+
+  render() {
+    return (
+      <div className="columns is-gapless">
+        <div className="column is-four-fifths has-text-right">
+          <input
+            placeholder="Search"
+            className="input is-rounded"
+            type="text"
+            name="search"
+            value={this.state.search}
+            onChange={this.handleInputChange}
+          />
+        </div>
+        <div className="column has-text-left">
+          <button className="button is-primary is-outlined" type="button" onClick={this.onClickSearch}>
+            <FaSearch></FaSearch>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,39 +1,45 @@
 import React from 'react';
-import { FaRocket } from 'react-icons/fa';
 
 import Layout from 'components/Layout';
 import Container from 'components/Container';
 import Columns from 'components/Columns';
 import Column from 'components/Column';
+import SearchBar from 'components/SearchBar';
 
-import img_gatsby_zurg from 'assets/images/gatsby-zurg.png';
+class IndexPage extends React.Component {
+  state = {
+    persons: []
+  };
 
-const IndexPage = () => {
+  onResultSearch = ( data ) => {
+    this.setState({ persons: data });
+  };
+
+  PersonsList = () => {
+    const listItems = this.state.persons.map(( p, i ) => <li key={i}>{ p.name }</li> );
+    return <ul>{ listItems }</ul>;
+  };
+
   // We don't include the title in Helmet here because we'll inherit the
   // default title from Layout
-  return (
-    <Layout pageName="home">
-      <Container className="content">
-        <Columns>
-          <Column>
-            <p className="gatsby-astronaut">
-              <img src={img_gatsby_zurg} alt="Build with Gatsby!" />
-            </p>
-            <h1>
-              <FaRocket /> Gatsby Zurg Starter
-            </h1>
-            <p>Welcome to your new Gatsby site.</p>
-            <p>Now go build something great.</p>
-            <h2>Still Getting Started?</h2>
-            <p>Run the following in your terminal!</p>
-            <pre>
-              <code>gatsby new [directory] https://github.com/colbyfayock/gatsby-starter-zurg</code>
-            </pre>
-          </Column>
-        </Columns>
-      </Container>
-    </Layout>
-  );
-};
+  render() {
+    return (
+      <Layout pageName="home">
+        <Container className="content">
+          <Columns>
+            <Column>
+              <SearchBar />
+            </Column>
+          </Columns>
+          <Columns>
+            <Column>
+              <this.PersonsList />
+            </Column>
+          </Columns>
+        </Container>
+      </Layout>
+    );
+  }
+}
 
 export default IndexPage;
